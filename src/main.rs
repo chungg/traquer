67052dd0b0bd1de3f5d846ed5bf4b7c8b05814b5
gord chung <5091603+chungg@users.noreply.github.com>
2024-05-16 11:56:28 -0400

    add stochastic and schaff indicators (#18)

--- src/main.rs
+++ src/main.rs
@@ -16,11 +16,5 @@ fn main() {
     let data = fs::read_to_string("./tests/rddt.input").expect("Unable to read file");
     let stats: SecStats = serde_json::from_str(&data).expect("JSON does not have correct format.");
 
-    dbg!(indicator::supertrend(
-        &stats.high,
-        &stats.low,
-        &stats.close,
-        16,
-        3.0
-    ));
+    dbg!(indicator::stc(&stats.close, 3, 6, 12));
 }
